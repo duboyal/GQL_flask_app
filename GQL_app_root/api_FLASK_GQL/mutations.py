220e9b36fd7ddb1f679d58c6f18c3cbea11ec80b
Alexandra Duboy <duboyal2@gmail.com>
2023-03-07 21:28:24 -0500

    Update mutations.py

--- GQL_app_root/api_FLASK_GQL/mutations.py
+++ GQL_app_root/api_FLASK_GQL/mutations.py
@@ -1,14 +1,84 @@
 from datetime import date
 from ariadne import convert_camel_case_to_snake, convert_kwargs_to_snake_case
+from ariadne import MutationType, make_executable_schema
 from api_FLASK_GQL import db
 from api_FLASK_GQL.models import Post
+import time
+from sqlalchemy import select, tuple_, or_, and_
+from sqlalchemy.sql import bindparam
+from sqlalchemy.sql.expression import literal
+
+
+@convert_kwargs_to_snake_case
+def create_mult_post_resolver(obj, info, posts):
+    try:
+        # Create a list of dictionaries containing the values for each row to be inserted
+        today = date.today()
+        rows = []
+        for post in posts:
+            rows.append({
+                'title': post['title'],
+                'description': post['description'],
+                'created_at': today.strftime("%m-%d-%Y") # 'created_at': post.get('created_at', created_at=today.strftime("%m-%d-%Y"))
+            })
+
+        # all_rows = Post.query.all()
+        stmt = select([Post.id, Post.title, Post.description])
+
+        #unique listings
+        stmt = select([Post.title, Post.description]).group_by(Post.title, Post.description)
+        unique_posts = db.session.execute(stmt).fetchall()
+        existing_tuples = [(item.title, item.description) for item in unique_posts]
+
+        # new_rows = rows
+        new_rows = [row for row in rows if (row['title'], row['description']) not in existing_tuples] #not in existing_tuples]
+
+        print(" ++++ new_rows")
+        print(new_rows)
+        print("unique_posts")
+        print(unique_posts)
+
+
+
+        if new_rows != []:
+            result = db.session.execute(Post.__table__.insert(), new_rows)
+            db.session.flush()
+            db.session.commit()
+
+            time.sleep(4)
+
+            ids = result.inserted_primary_key_rows
+            # ids = [row[0] for row in result.inserted_primary_key_rows]
+            print(ids)
+            print(rows) #new_rows ??
+
+            # Create a list of Post objects representing the newly created posts, setting the ids on the objects
+            post_list = []
+            for i, row in enumerate(rows):
+                post_list.append(Post(id = ids[i], title=row['title'], description=row['description'], created_at=row['created_at']))
+
+
+        payload = {
+            "success": True,
+
+            "posts" : new_rows #post_list
+        }
+
+    except ValueError as e:
+        payload = {
+            "success": False,
+            "errors": [str(e)]
+        }
+    return payload
+#~~~~~~~~~~~~~~~
+
 
 # @convert_camel_case_to_snake
 @convert_kwargs_to_snake_case
 def create_post_resolver(obj, info, title, description):
     try:
         today = date.today()
-        post = Post(title=title, description=description, created_at=today.strftime("%d-%d-%Y"))
+        post = Post(title=title, description=description, created_at=today.strftime("%m-%d-%Y"))
 
         db.session.add(post)
         db.session.commit()
@@ -71,7 +141,6 @@ def delete_post_resolver(obj, info, id): #this one requires an id !
 # -------------------//----------------
 # #maybe its too dangerous and i'd rather somehow 
 # make a query that would maybe find each id in a range and pass if there are none ?
-
 # @convert_kwargs_to_snake_case
 # def delete_all_posts(obj, info):
 #     try:
